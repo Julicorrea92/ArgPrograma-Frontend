50489e2462551e13ab4e0eef75be08ba3f6a204c
Julian <julianleo92@hotmail.com>
2022-08-16 21:54:33 -0300

    Frontend estatico con Angular

---
+++ Proyecto Angular/portfolioJulianCorrea/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AcercaDeMiComponent } from './componentes/acerca-de-mi/acerca-de-mi.component';
+import { ExperienciaLaboralComponent } from './componentes/experiencia-laboral/experiencia-laboral.component';
+import { FormacionAcademicaComponent } from './componentes/formacion-academica/formacion-academica.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardYSoftSkillsComponent } from './componentes/hard-y-soft-skills/hard-y-soft-skills.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AcercaDeMiComponent,
+    ExperienciaLaboralComponent,
+    FormacionAcademicaComponent,
+    HardYSoftSkillsComponent,
+    FooterComponent,
+    ProyectosComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Proyecto Angular/portfolioJulianCorrea/src/app/componentes/hard-y-soft-skills/hard-y-soft-skills.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hard-y-soft-skills',
+  templateUrl: './hard-y-soft-skills.component.html',
+  styleUrls: ['./hard-y-soft-skills.component.css']
+})
+export class HardYSoftSkillsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
